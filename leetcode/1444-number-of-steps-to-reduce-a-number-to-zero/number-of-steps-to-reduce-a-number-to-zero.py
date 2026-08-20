8dd13ace63ccbe15ce6a79ef0dcb1716830df2db
SUJAY KANDULA <sujaykandula@gmail.com>
2026-08-20 16:57:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (90.35%) - LeetSync

---
+++ leetcode/1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        steps=0
+        while num!=0:
+            if num%2==0:
+                num=num//2
+            else:
+                num=num-1
+            steps=steps+1
+        return steps
+        
+        
